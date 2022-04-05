9c7dafda2a915fce1c164afbeb57c9f31fb7d2a9
Georgii <khomaldi@yandex.ru>
2022-04-05 23:53:54 +0300

    correct urls, style refactor

--- pages/login.php
+++ pages/login.php
@@ -2,25 +2,28 @@
 include(__DIR__ . '/../bootstrap.php');
 
 $users = [
-    'Slaven' => '$2y$10$jh9br7M./dxkehxYnnYt3epxE8cYneQZgOn0IcJ.ZDWJear11xNSK',
-    'TestUser1' => '$2y$10$H099GP.Y9Ja3Q1fLe3y0CexYbCuj59RMnTOUOa.6JjliYt40QKH7m'
+    'Slaven'    => '$2y$10$jh9br7M./dxkehxYnnYt3epxE8cYneQZgOn0IcJ.ZDWJear11xNSK',
+    'TestUser1' => '$2y$10$H099GP.Y9Ja3Q1fLe3y0CexYbCuj59RMnTOUOa.6JjliYt40QKH7m',
 ];
 
 if (isset($_POST['username'], $_POST['password'])) {
-// The user has submitted the login form
+    // The user has submitted the login form
     $expectedPasswordHash = $users[$_POST['username']];
+
     if (password_verify($_POST['password'], $expectedPasswordHash)) {
-// The provided password is also correct
-// Remember the username of the user who just logged in
+        // The provided password is also correct
+        // Remember the username of the user who just logged in
         $_SESSION['authenticated_user'] = $_POST['username'];
+
         // Redirect to /tasks.php
-        header('Location: /../tasks');
+        header('Location: /create-task');
+
         exit;
     }
 }
+
 $title = 'Login';
-?>
-<?php
+
 include(__DIR__ . '/../_header.php');
 ?>
 
@@ -41,4 +44,5 @@ include(__DIR__ . '/../_header.php');
         <button type="submit">Submit</button>
     </div>
 </form>
+
 <?php include(__DIR__ . '/../_footer.php'); ?>
